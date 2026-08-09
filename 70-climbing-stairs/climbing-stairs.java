5ac1e8ed21d0b317e6dc41e7b5e98617f508c644
koti-galam <galam.v.koti@gmail.com>
2026-08-09 23:48:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (17.82%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=2)
+        {
+            return n;
+        }
+        
+        int prev0=1;
+        int prev1=2;
+        for(int i=3;i<n;i++)
+        {
+            int temp=prev0+prev1;
+            prev0=prev1;
+            prev1=temp;
+        }
+        return prev0+prev1;
+    }
+}
